1f4bb7c53b98058387db1f1dac29f58ef820a232
Liam Searle <liamsearle7@gmail.com>
2019-02-21 15:53:30 +0200

    Adding Problems.Tests

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,40 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(prefix("what    ...  did you say?? "));//input string
         }
+        static string prefix(string input){
+            if(input == null){  //error prevention
+                return null;
+            }
+            if (input == "")
+            {
+                return "0,0:";
+            }
+            int length = input.Length;
+           
+           
+            int wordCount = 0, index = 0;
+
+            while (index < input.Length)//count the number of words
+            {
+            // check if current char is part of a word
+            while (index < input.Length && !char.IsWhiteSpace(input[index]))
+                index++;
+
+            wordCount++;
+
+            // skip whitespace until next word
+            while (index < input.Length && char.IsWhiteSpace(input[index]))
+                index++;
+            }
+            
+            return length + "," + wordCount + ":" + input;
+        }
+        
     }
 }
